493d697b779dec51d301b8f902814a8d5580aa12
Wind Pham (HKO - CORPIT - AS - CTO) <techvify34@talapparel.com>
2025-12-12 13:02:47 +0700

    tmp

--- app/musics/(tabs)/_layout.tsx
+++ app/musics/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import {colors, fontSize} from '@/constants/tokens'
-import {FontAwesome, FontAwesome6, Ionicons, MaterialCommunityIcons} from '@expo/vector-icons'
+import {FontAwesome6, Ionicons, MaterialCommunityIcons} from '@expo/vector-icons'
 import {BlurView} from 'expo-blur'
 import {Tabs} from 'expo-router'
 import {StyleSheet} from 'react-native'
