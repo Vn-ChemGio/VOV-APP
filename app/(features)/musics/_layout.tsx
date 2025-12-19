f9357bd5abd2c8e6a4e27cbf22c081488e382f8c
Wind Pham (HKO - CORPIT - AS - CTO) <techvify34@talapparel.com>
2025-12-19 22:01:00 +0700

    tmp

--- app/(features)/musics/_layout.tsx
+++ app/(features)/musics/_layout.tsx
@@ -1,11 +1,14 @@
-import {colors, fontSize} from '@/constants/tokens'
-import {FontAwesome6, Ionicons, MaterialCommunityIcons} from '@expo/vector-icons'
+import React from "react";
+import {Platform, StyleSheet} from 'react-native'
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {PlatformPressable} from "@react-navigation/elements";
+import * as Haptics from 'expo-haptics';
 import {BlurView} from 'expo-blur'
 import {Tabs} from 'expo-router'
-import {StyleSheet} from 'react-native'
+
+import {colors, fontSize} from '@/constants/tokens'
+import {FontAwesome6, Ionicons, MaterialCommunityIcons} from '@expo/vector-icons'
 import {AudioFloatingPlayer} from "@/views/audio-player";
-import React from "react";
-import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 const TabsNavigation = () => {
   const {bottom} = useSafeAreaInsets();
@@ -20,21 +23,49 @@ const TabsNavigation = () => {
             fontWeight: '500',
           },
           headerShown: false,
-          tabBarStyle: {
-            position: 'absolute',
-            borderTopLeftRadius: 20,
-            borderTopRightRadius: 20,
-            borderTopWidth: 0,
-            paddingTop: 8,
+          tabBarStyle: Platform.select({
+            ios: {
+              // Use a transparent background on iOS to show the blur effect
+              position: 'absolute',
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+              borderTopWidth: 0,
+              paddingTop: 8,
+            },
+            default: {
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+              borderTopWidth: 0,
+            },
+          }),
+          tabBarBackground: () => {
+            if (Platform.OS === 'ios') {
+              return (
+                <BlurView
+                  tint='systemChromeMaterial'
+                  intensity={100}
+                  style={{
+                    ...StyleSheet.absoluteFillObject,
+                    overflow: 'hidden',
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                  }}
+                />
+              );
+            }
+            
+            // On Android & Web: no background
+            return null;
           },
-          tabBarBackground: () => (
-            <BlurView
-              intensity={95}
-              style={{
-                ...StyleSheet.absoluteFillObject,
-                overflow: 'hidden',
-                borderTopLeftRadius: 20,
-                borderTopRightRadius: 20,
+          tabBarButton: (props) => (
+            <PlatformPressable
+              {...props}
+              onPressIn={(ev) => {
+                if (process.env.EXPO_OS === 'ios') {
+                  // Add a soft haptic feedback when pressing down on the tabs.
+                  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                }
+                props.onPressIn?.(ev);
               }}
             />
           ),
